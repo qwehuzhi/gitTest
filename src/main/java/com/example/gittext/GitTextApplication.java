4dc5f64bca9360e88b96eb4e4d08b93839c8ca3b
胡志 <1780024678@qq.com>
2023-05-27 20:03:25 +0800

    首次提交

---
+++ src/main/java/com/example/gittext/GitTextApplication.java
@@ -0,0 +1,13 @@
+package com.example.gittext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTextApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gittext/GitTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gittext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
